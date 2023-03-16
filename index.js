1d6527c8c708323b353e0de46210285d017a95b7
Evgeny Yarusov <yarusovea@gmail.com>
2023-03-16 12:15:00 +0500

    homework done. magic btn can remove cloneBtn

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ btn.addEventListener('click', () => {
   // при нажатии на первую кнопку
   // работайте с clonedBtn
   // ваш код начинается здесь
-  
+  if (document.body.lastChild === clonedBtn) {
+    document.body.removeChild(clonedBtn);
+  } else {
+    clonedBtn.innerText = "Я изменю тебя";
+    document.body.appendChild(clonedBtn);
+    clonedBtn.style.backgroundColor = "#a78b71";
+    clonedBtn.style.color = "white";
+    clonedBtn.style.fontSize = "20px";
+    clonedBtn.style.border = "none";
+    clonedBtn.style.padding = "20px";
+  }
 })
 
 // здесь можете создать EventListener для второй кнопки
+clonedBtn.addEventListener('click', () => {
+  btn.style.backgroundColor = "#9c4a1a";
+  btn.style.color = "black";  
+})
